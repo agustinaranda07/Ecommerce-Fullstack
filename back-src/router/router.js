6008542abce728232c67f4d84dba0d6d0e680bd7
Bruno Maciel <brunomacielcf@gmail.com>
2023-02-03 23:00:41 -0300

    ABM de usuarios completo

--- back-src/router/router.js
+++ back-src/router/router.js
@@ -4,65 +4,147 @@ const mysqlConnection = require("../database/database");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
 //Ruta raiz
 router.get("/", (req, res) => {
   res.send("<h1>Hi!</h1>");
 });
 
-
-// ruta pedir todos los usuarios (sin autenticar)
-router.get("/usuarios", (req, res) => {
-  const query = "select * from usuarios";
-    mysqlConnection.query(query, (err, rows) => {
-      if (!err) {
-        res.json({
-          status: true,
-          datos: rows,
-        });
-      } else {
-        res.json({
-          status: false,
-          message: "Server error",
-        });
-        console.log(err);
-      }
-    });
+// ruta pedir todos los usuarios
+router.get("/usuarios", tokenVerifier, (req, res) => {
+  jwt.verify(req.token, "ecommerceKey", (err, valido) => {
+    if (err) {
+      res.json({
+        status: false,
+        message: "Error 403",
+      });
+      res.sendStatus(403);
+    } else {
+      const query = "SELECT * FROM usuarios WHERE estado='A'";
+      mysqlConnection.query(query, (err, rows) => {
+        if (!err) {
+          res.json({
+            status: true,
+            datos: rows,
+          });
+        } else {
+          res.json({
+            status: false,
+            message: "Server error",
+          });
+          console.log(err);
+        }
+      });
+    }
+  });
 });
 
-//Ruta pedir usuario por ID (sin autenticar)
-router.get(`/usuarios/:id_usuario`,(req,res)=>{
+//Ruta pedir usuario por ID
+router.get(`/usuarios/:id_usuario`, tokenVerifier, (req, res) => {
   const { id_usuario } = req.params;
-  const query = `select * from usuarios where id_usuario=${id_usuario}`;
-  mysqlConnection.query(query,(err,rows)=>{
-    if(!err){
-      if (rows.lenght != 0){
-        res.json({
-          status:true,
-          datos:rows,
-        });
-      }else{
-        res.json({
-          status:false,
-          message: "ID no encontrado"
-        })
-      }
-    }else{
+  const query = `SELECT * FROM usuarios WHERE estado='A' AND id_usuario=${id_usuario}`;
+  jwt.verify(req.token, "ecommerceKey", (err, valido) => {
+    if (err) {
       res.json({
-        status:false,
-        message:"Server error"
+        status: false,
+        message: "Error 403",
       });
-      console.log(err)
+      res.sendStatus(403);
+    } else {
+      mysqlConnection.query(query, (err, rows) => {
+        if (!err) {
+          if (rows.length != 0) {
+            res.json({
+              status: true,
+              datos: rows,
+            });
+          } else {
+            res.json({
+              status: false,
+              message: "ID no encontrado",
+            });
+          }
+        } else {
+          res.json({
+            status: false,
+            message: "Server error",
+          });
+          console.log(err);
+        }
+      });
     }
-  })
-})
+  });
+});
+
+// ruta modificar datos de usuario;
+router.put("/usuarios/:id_usuario", tokenVerifier, (req, res) => {
+  const { id_usuario } = req.params;
+  let {email, nombre, apellido} = req.body;
+  jwt.verify(req.token, "ecommerceKey", (err, valido) => {
+    if (err) {
+      res.json({
+        status: false,
+        message: "Error 403",
+      });
+      res.sendStatus(403);
+    } else {
+      let query = `UPDATE usuarios SET email='${email}', nombre='${nombre}', apellido='${apellido}' WHERE id_usuario = '${id_usuario}'`;
+      mysqlConnection.query(query, (err, rows) => {
+        if (!err) {
+          res.json({
+            status: true,
+            message: "Los datos fueron actualizados con exito",
+          });
+          console.log(
+            `ID editado:${id_usuario}`
+          );
+        } else {
+          res.json({
+            status: false,
+            message: "Server error",
+          });
+          console.log(err);
+        }
+      });
+    }
+  });
+});
+
+// ruta dar de baja usuario (logico)
+router.delete(`/usuarios/:id_usuario`, tokenVerifier, (req, res) => {
+  let { id_usuario } = req.params;
+  jwt.verify(req.token, "ecommerceKey", (err, valido) => {
+    if (err) {
+      res.json({
+        status: false,
+        message: "Error 403",
+      });
+      res.sendStatus(403);
+    } else {
+      let query = `UPDATE usuarios SET estado='B' WHERE id_usuario ='${id_usuario}'`;
+      mysqlConnection.query(query, (err, rows) => {
+        if (!err) {
+          res.json({
+            status: true,
+            message: "El usuario fue dado de baja",
+          });
+        } else {
+          res.json({
+            status: false,
+            message: "Server error",
+          });
+          console.log(err);
+        }
+      });
+    }
+  });
+});
 
 // ruta login
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
   if (email != undefined && password != undefined) {
     mysqlConnection.query(
-      'select u.id_usuario,  u.password,  u.email, u.apellido, u.nombre from usuarios u where u.email=?',
+      "select u.id_usuario,  u.password,  u.email, u.apellido, u.nombre from usuarios u WHERE estado='A' AND u.email=?",
       [email],
       (err, rows) => {
         if (!err) {
@@ -114,8 +196,7 @@ router.post("/login", (req, res) => {
 
 // ruta registro
 router.post("/signin", (req, res) => {
-  const { email, password, nombre, apellido } =
-    req.body;
+  const { email, password, nombre, apellido } = req.body;
   var hash = bcrypt.hashSync(password, 10);
   let query = `INSERT INTO usuarios(email, password, nombre, apellido)VALUES('${email}','${hash}','${nombre}','${apellido}')`;
   mysqlConnection.query(query, (err, rows) => {
@@ -137,7 +218,7 @@ router.post("/signin", (req, res) => {
 // ruta resetear password
 router.put("/resetpassword/:id_usuario", (req, res) => {
   const { id_usuario } = req.params;
-  let {password} = req.body;
+  let { password } = req.body;
   var hash = bcrypt.hashSync(password, 10);
   let query = `UPDATE usuarios SET password='${hash}' WHERE id_usuario = '${id_usuario}'`;
   mysqlConnection.query(query, (err, rows) => {
